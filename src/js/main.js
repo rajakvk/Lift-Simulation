1826713506b0b3a59d7ee36bc1e553a141bd013b
rajakvk <rajakvk@gmail.com>
2021-08-18 18:35:34 +0530

    [vanilla-user] converted as react app with user configuration

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,97 @@
-;(function() {
-  var lift,
-  init = () => {
-    lift = document.querySelector('.lift');
-  },
-  moveLift = (e) => {
-    const floor = e.target.dataset['floor'];
-    const direction = e.target.dataset['direction'];
-    switch (floor) {
-      case "1":
-        lift.style.cssText = `bottom: 0px;`;
-        break;
-      case "2":
-        lift.style.cssText = `bottom: 100px;`;
-        break;
-      case "3":
-        lift.style.cssText = `bottom: 200px;`;
-        break;
-      case "4":
-        lift.style.cssText = `bottom: 300px;`;
-        break;
-      default:
-        // left intentionally
-    }
-  },
-  bindEvents = () => {
-    document.querySelectorAll('.button-container .button').forEach(element => {
-      element.addEventListener('click', moveLift);
-    });
+const Configuration = ({buildFloors, setLifts}) => {
+  const { useRef } = React;
+  const floorsRef = useRef(null);
+  const liftsRef = useRef(null);
+  const build = () => {
+    buildFloors(Number(floorsRef.current.value)+1); 
+    setLifts(Number(liftsRef.current.value));
   };
+  return (
+    <section>
+      <div className="simulation-config">
+        <fieldset>
+          <legend>Simulation Configuration</legend>
+          <label htmlFor="floors">
+            Floors:
+            <input ref={floorsRef} min="2" max="50" type="number" name="floors" id="floors" defaultValue="4" />
+          </label>
+          <label htmlFor="lifts">
+            Lifts:
+            <input disabled ref={liftsRef} type="number" name="lifts" id="lifts" defaultValue="1" />
+          </label>
+          <button onClick={build}>Rebuild</button>
+        </fieldset>
+      </div>
+    </section>
+  )
+};
 
-  document.addEventListener('DOMContentLoaded', (event) => {
-    init();
-    bindEvents();
-  });
-})();
+const moveLift = (e) => {
+  const lift = document.querySelector('.lift');
+  const floor = e.target.dataset['floor'];
+  const direction = e.target.dataset['direction'];
+  const multiplier = Number(floor-1) * 100;
+  lift.style.cssText = `bottom: ${multiplier}px;`;
+}
+
+const Building = ({heading, floors, lifts}) => {
+  (floors = floors).shift();
+  (floors = floors).reverse();
+  return (
+    <section>
+      <h2>{heading}</h2>
+      <div className="building">
+        <div className="floor-container">
+
+          {
+            floors.map( item => {
+              return(
+                <div className={`floor floor-${item}`}>
+                  <div className="button-container">
+                    <button onClick={moveLift} data-floor={`${item}`} data-direction="up" className="button button-up">Up</button>
+                    <button onClick={moveLift} data-floor={`${item}`} data-direction="down" className="button button-down">Down</button>
+                  </div>
+                  <span className="board">Floor {item}</span>
+                </div>
+              )}
+            )
+          }
+        
+        </div>
+        <div className="lift-container">
+          {
+            lifts.map(item => {
+              return (
+                <div className="lift"></div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </section>
+  )
+};
+
+const Simulation = () => {
+  const { useState } = React;
+  const [floors, setFloors] = useState(5);
+  const [lifts, setLifts] = useState(1);
+  return (
+    <>
+      <Configuration
+        buildFloors={setFloors}
+        setLifts={setLifts}
+      />
+      <Building 
+        heading="Building" 
+        floors={[ ...Array(floors).keys() ] } 
+        lifts={[ ...Array(lifts).keys() ] }
+      />
+    </>
+  )
+};
+
+ReactDOM.render(
+  <Simulation />,
+  document.getElementById('app')
+);
